e8645e7cef10ce346d3e4557d6efb254724ff142
Matteo Pierro <pierromatteo@gmail.com>
2023-11-19 11:28:31 +0200

    add indoc as dependency

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,11 @@
 #[cfg(test)]
 mod tests {
-    use crate::input_reader::read_lines;
+    use crate::input_reader::*;
 
     #[test]
     fn it_works() {
-        let lines = read_lines("test.txt");
+        let input = read_input_file("test.txt");
+        let lines = read_lines(&input);
         assert_eq!(vec!["some text", "here and", "there"], lines);
     }
 }
--- src/input_reader.rs
+++ src/input_reader.rs
@@ -2,22 +2,35 @@ use std::fs;
 use std::path::PathBuf;
 
 pub fn read_lines(input: &str) -> Vec<String> {
+    input.lines().map(String::from).collect()
+}
+
+pub fn read_input_file(filename: &str) -> String {
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("inputs")
-        .join(input);
-    fs::read_to_string(&path)
-        .map(|c|
-            c.lines().map(String::from).collect()
-        ).unwrap()
+        .join(filename);
+    fs::read_to_string(&path).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
+    use indoc::indoc;
     use super::*;
 
     #[test]
-    fn it_reads_input_lines() {
-        let lines = read_lines("test.txt");
+    fn it_reads_input_file() {
+        let input = read_input_file("test.txt");
+        let lines = read_lines(&input);
         assert_eq!(vec!["some text", "here and", "there"], lines);
+    }
+
+    #[test]
+    fn it_reads_lines() {
+        let input = indoc! {"
+        foo bar
+        fizz
+        bazz"};
+        let lines = read_lines(&input);
+        assert_eq!(vec!["foo bar", "fizz", "bazz"], lines);
     }
 }
