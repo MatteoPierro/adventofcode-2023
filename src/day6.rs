68e24ccd26f5c47267908a673bf3b33b6ef3c6f0
Matteo Pierro <pierromatteo@gmail.com>
2023-12-05 19:23:53 +0200

    scaffold day 6

---
+++ src/day6.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    #[test]
+    fn it_does_something() {
+        let input = indoc! {"
+        something here
+        and here"};
+
+        assert_eq!("", input);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,4 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
