b6094fa59fc9a526ced40028862d0eaabdb6c993
Matteo Pierro <pierromatteo@gmail.com>
2023-12-06 08:34:49 +0200

    it solves first part day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,13 +1,39 @@
+fn calculate_record_breaks(times: Vec<usize>, distance: Vec<usize>) -> usize {
+    times.iter().zip(distance)
+        .map(|(&t, d)| calculate_winning_combinations(t, d))
+        .fold(1, |curr, acc| acc * curr)
+}
+
+fn calculate_winning_combinations(time: usize, distance: usize) -> usize {
+    let time_range = 1..time;
+    let distance_range = time_range.clone().rev();
+    time_range.zip(distance_range)
+        .map(|(t, d)| t * d).filter(|&s| s > distance)
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
-    use indoc::indoc;
+    use crate::day6::*;
 
     #[test]
-    fn it_does_something() {
-        let input = indoc! {"
-        something here
-        and here"};
+    fn it_solves_first_part() {
+        let times = vec![56,71,79,99];
+        let distance = vec![334, 1135, 1350, 2430];
+        assert_eq!(211904, calculate_record_breaks(times, distance));
+    }
 
-        assert_eq!("", input);
+    #[test]
+    fn it_calculates_record_breaks() {
+        let times = vec![7, 15, 30];
+        let distance = vec![9, 40, 200];
+        assert_eq!(288, calculate_record_breaks(times, distance));
+    }
+
+    #[test]
+    fn it_calculates_calculate_winning_combinations() {
+        assert_eq!(4, calculate_winning_combinations(7, 9));
+        assert_eq!(8, calculate_winning_combinations(15, 40));
+        assert_eq!(9, calculate_winning_combinations(30, 200));
     }
 }
