f6074598cc3e7f6be0a961171d416e9098e5e217
Matteo Pierro <pierromatteo@gmail.com>
2023-12-12 13:30:24 +0200

    use recursive algorithm

--- src/day12.rs
+++ src/day12.rs
@@ -2,34 +2,6 @@ use std::collections::LinkedList;
 
 use crate::input_reader::read_lines;
 
-fn expand_spring(s: &str) -> Vec<String> {
-    let mut to_expand: LinkedList<String> = LinkedList::from([s.to_string()]);
-    let mut expanded: Vec<String> = vec![];
-
-    while !to_expand.is_empty() {
-        let s = to_expand.pop_back().unwrap();
-        if !s.contains("?") {
-            expanded.push(s);
-            continue;
-        }
-
-        to_expand.push_back(s.replacen("?", ".", 1).to_string());
-        to_expand.push_back(s.replacen("?", "#", 1).to_string());
-    }
-    expanded
-}
-
-fn is_valid_spring(s: &str, size_continuous_damaged_groups: &Vec<usize>) -> bool {
-    let pairs = s.split('.').filter(|&s| !s.is_empty()).map(|l| l.len()).collect::<Vec<_>>();
-    // print!("{:?}", pairs);
-    if pairs.len() != size_continuous_damaged_groups.len() {
-        return false;
-    }
-    pairs.iter()
-        .zip(size_continuous_damaged_groups)
-        .all(|(&a, &b)| a == b)
-}
-
 fn numbers_of_valid_springs(input: &str) -> Vec<usize> {
     read_lines(input).iter()
         .map(|s| {
@@ -43,12 +15,69 @@ fn numbers_of_valid_springs(input: &str) -> Vec<usize> {
 }
 
 fn number_of_valid_springs(s: &str, size_continuous_damaged_groups: &Vec<usize>) -> usize {
-    let expanded = expand_spring(s);
+    let mut spring = s.chars().collect::<LinkedList<_>>();
+    let mut groups_size = size_continuous_damaged_groups.iter().map(|i| *i).collect::<LinkedList<usize>>();
+    return calculate_number_of_damaged_springs(&mut spring, &mut groups_size, 0);
+}
 
-    // println!("{:?}", expanded);
-    // println!();
+fn calculate_number_of_damaged_springs(spring: &mut LinkedList<char>, groups_size: &mut LinkedList<usize>, damages_springs_counter: usize) -> usize {
+    if spring.is_empty() && damages_springs_counter != 0 { // we saw damaged springs
+        return if groups_size.len() == 1 && damages_springs_counter == groups_size.pop_front().unwrap() {
+            1
+        } else {
+            0
+        };
+    }
 
-    expanded.iter().filter(|s| is_valid_spring(s, &size_continuous_damaged_groups)).count()
+    if spring.is_empty() && damages_springs_counter == 0 { // we never saw damaged springs
+        return if groups_size.is_empty() { // it wasn't supposed to have damaged springs
+            1
+        } else {
+            0
+        };
+    }
+
+    if damages_springs_counter > 0 && groups_size.is_empty() { // we saw more damaged spring than expected
+        return 0;
+    }
+
+    if damages_springs_counter > 0 && damages_springs_counter > *groups_size.front().unwrap() { // we saw more damaged spring than expected
+        return 0;
+    }
+
+    let next = spring.pop_front().unwrap();
+
+    match next {
+        '#' => calculate_number_of_damaged_springs(spring, groups_size, damages_springs_counter + 1),
+
+        '.' => { // arrived at the end of a chunk
+            if damages_springs_counter > 0 { // we saw damaged springs
+                let expected_size = groups_size.pop_front().unwrap();
+                if damages_springs_counter != expected_size {
+                    return 0;
+                }
+            }
+            calculate_number_of_damaged_springs(spring, groups_size, 0)
+        }
+
+        _ => {
+            if groups_size.is_empty() { // we can continue checking if we find damaged springs or not
+                return calculate_number_of_damaged_springs(spring, groups_size, 0);
+            }
+
+            if *groups_size.front().unwrap() == damages_springs_counter { // we found a valid group of damaged springs let's move on
+                groups_size.pop_front();
+                return calculate_number_of_damaged_springs(spring, groups_size, 0);
+            }
+
+            if damages_springs_counter > 0 { // we are still counting a spring group. let's suppose that ? is a #.
+                return calculate_number_of_damaged_springs(spring, groups_size, damages_springs_counter + 1);
+            }
+
+            calculate_number_of_damaged_springs(&mut spring.clone(), &mut groups_size.clone(), damages_springs_counter + 1)
+                + calculate_number_of_damaged_springs(&mut spring.clone(), &mut groups_size.clone(), damages_springs_counter)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -59,7 +88,6 @@ mod tests {
     use crate::input_reader::*;
 
     #[test]
-    #[ignore] // it's slow. runs in 14 seconds
     fn it_solves_first_part() {
         let input = read_input_file("input_day12.txt");
 
@@ -78,6 +106,11 @@ mod tests {
 
         assert_eq!(vec![1, 4, 1, 1, 4, 10], numbers_of_valid_springs(input));
         assert_eq!(21, numbers_of_valid_springs(input).iter().sum::<usize>());
+    }
+
+    #[test]
+    fn it_calculates_number_of_damaged_springs() {
+        assert_eq!(1, calculate_number_of_damaged_springs(&mut "???.###".chars().collect::<LinkedList<_>>(), &mut vec![1, 1, 3].iter().map(|i| *i).collect::<LinkedList<usize>>(), 0));
     }
 
     #[test]
