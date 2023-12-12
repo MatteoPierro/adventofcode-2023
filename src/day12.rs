18f5a1bbe0f8b04494eba0e81a99a3a84d8b6cd0
Matteo Pierro <pierromatteo@gmail.com>
2023-12-12 13:59:16 +0200

    it solves second part day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,28 +1,36 @@
 use std::collections::LinkedList;
 
+use memoize::memoize;
+
 use crate::input_reader::read_lines;
 
-fn numbers_of_valid_springs(input: &str) -> Vec<usize> {
+fn numbers_of_valid_springs(input: &str, expand: bool) -> Vec<usize> {
     read_lines(input).iter()
         .map(|s| {
             let parts: Vec<_> = s.split(" ").collect();
-            let spring = parts[0].to_string();
-            let size_continuous_damaged_groups: Vec<_> = parts[1].split(",").map(|s| s.parse::<usize>().unwrap()).collect();
-            // println!("{:?}", spring);
-            // println!("{:?}", size_continuous_damaged_groups);
+            let mut spring = parts[0].to_string();
+            let mut size_continuous_damaged_groups: Vec<_> = parts[1].split(",").map(|s| s.parse::<usize>().unwrap()).collect();
+            if expand {
+                spring = std::iter::repeat(spring).take(5).collect::<Vec<_>>().join("?");
+                size_continuous_damaged_groups = size_continuous_damaged_groups.iter().cycle()
+                    .take(size_continuous_damaged_groups.len() * 5)
+                    .cloned().
+                    collect();
+            }
             number_of_valid_springs(&spring, &size_continuous_damaged_groups)
         }).collect()
 }
 
 fn number_of_valid_springs(s: &str, size_continuous_damaged_groups: &Vec<usize>) -> usize {
-    let mut spring = s.chars().collect::<LinkedList<_>>();
-    let mut groups_size = size_continuous_damaged_groups.iter().map(|i| *i).collect::<LinkedList<usize>>();
-    return calculate_number_of_damaged_springs(&mut spring, &mut groups_size, 0);
+    let spring = s.chars().collect::<LinkedList<_>>();
+    let groups_size = size_continuous_damaged_groups.iter().map(|i| *i).collect::<LinkedList<usize>>();
+    return calculate_number_of_valid_springs(spring, groups_size, 0);
 }
 
-fn calculate_number_of_damaged_springs(spring: &mut LinkedList<char>, groups_size: &mut LinkedList<usize>, damages_springs_counter: usize) -> usize {
+#[memoize]
+fn calculate_number_of_valid_springs(spring: LinkedList<char>, groups_size: LinkedList<usize>, damages_springs_counter: usize) -> usize {
     if spring.is_empty() && damages_springs_counter != 0 { // we saw damaged springs
-        return if groups_size.len() == 1 && damages_springs_counter == groups_size.pop_front().unwrap() {
+        return if groups_size.len() == 1 && damages_springs_counter == *groups_size.front().unwrap() {
             1
         } else {
             0
@@ -45,37 +53,40 @@ fn calculate_number_of_damaged_springs(spring: &mut LinkedList<char>, groups_siz
         return 0;
     }
 
+    let mut spring = spring;
     let next = spring.pop_front().unwrap();
 
     match next {
-        '#' => calculate_number_of_damaged_springs(spring, groups_size, damages_springs_counter + 1),
+        '#' => calculate_number_of_valid_springs(spring, groups_size, damages_springs_counter + 1),
 
         '.' => { // arrived at the end of a chunk
+            let mut groups_size = groups_size;
             if damages_springs_counter > 0 { // we saw damaged springs
                 let expected_size = groups_size.pop_front().unwrap();
                 if damages_springs_counter != expected_size {
                     return 0;
                 }
             }
-            calculate_number_of_damaged_springs(spring, groups_size, 0)
+            calculate_number_of_valid_springs(spring, groups_size, 0)
         }
 
-        _ => {
+        _ => { // it it's a ?
             if groups_size.is_empty() { // we can continue checking if we find damaged springs or not
-                return calculate_number_of_damaged_springs(spring, groups_size, 0);
+                return calculate_number_of_valid_springs(spring, groups_size, 0);
             }
 
             if *groups_size.front().unwrap() == damages_springs_counter { // we found a valid group of damaged springs let's move on
+                let mut groups_size = groups_size;
                 groups_size.pop_front();
-                return calculate_number_of_damaged_springs(spring, groups_size, 0);
+                return calculate_number_of_valid_springs(spring, groups_size, 0);
             }
 
             if damages_springs_counter > 0 { // we are still counting a spring group. let's suppose that ? is a #.
-                return calculate_number_of_damaged_springs(spring, groups_size, damages_springs_counter + 1);
+                return calculate_number_of_valid_springs(spring, groups_size, damages_springs_counter + 1);
             }
 
-            calculate_number_of_damaged_springs(&mut spring.clone(), &mut groups_size.clone(), damages_springs_counter + 1)
-                + calculate_number_of_damaged_springs(&mut spring.clone(), &mut groups_size.clone(), damages_springs_counter)
+            calculate_number_of_valid_springs(spring.clone(), groups_size.clone(), damages_springs_counter + 1)
+                + calculate_number_of_valid_springs(spring.clone(), groups_size.clone(), damages_springs_counter)
         }
     }
 }
@@ -88,10 +99,12 @@ mod tests {
     use crate::input_reader::*;
 
     #[test]
+    #[ignore] // completes in more than 16 seconds
     fn it_solves_first_part() {
         let input = read_input_file("input_day12.txt");
 
-        assert_eq!(7251, numbers_of_valid_springs(&input).iter().sum::<usize>());
+        assert_eq!(7251, numbers_of_valid_springs(&input, false).iter().sum::<usize>());
+        assert_eq!(2128386729962, numbers_of_valid_springs(&input, true).iter().sum::<usize>());
     }
 
     #[test]
@@ -104,13 +117,15 @@ mod tests {
         ????.######..#####. 1,6,5
         ?###???????? 3,2,1"};
 
-        assert_eq!(vec![1, 4, 1, 1, 4, 10], numbers_of_valid_springs(input));
-        assert_eq!(21, numbers_of_valid_springs(input).iter().sum::<usize>());
+        assert_eq!(vec![1, 4, 1, 1, 4, 10], numbers_of_valid_springs(input, false));
+        assert_eq!(vec![1, 16384, 1, 16, 2500, 506250], numbers_of_valid_springs(input, true));
+        assert_eq!(21, numbers_of_valid_springs(input, false).iter().sum::<usize>());
+        assert_eq!(525152, numbers_of_valid_springs(input, true).iter().sum::<usize>());
     }
 
     #[test]
     fn it_calculates_number_of_damaged_springs() {
-        assert_eq!(1, calculate_number_of_damaged_springs(&mut "???.###".chars().collect::<LinkedList<_>>(), &mut vec![1, 1, 3].iter().map(|i| *i).collect::<LinkedList<usize>>(), 0));
+        assert_eq!(1, calculate_number_of_valid_springs("???.###".chars().collect::<LinkedList<_>>(), vec![1, 1, 3].iter().map(|i| *i).collect::<LinkedList<usize>>(), 0));
     }
 
     #[test]
