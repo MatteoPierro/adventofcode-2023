cad091ea39611531295628daadb9d768bf5f5501
Matteo Pierro <pierromatteo@gmail.com>
2023-12-20 21:54:11 +0100

    scaffold day 21

---
+++ src/day21.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    #[test]
+    fn it_does_something() {
+        let input = indoc! {"
+        something here
+        and here"};
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,4 @@ mod day17;
 mod day18;
 mod day19;
 mod day20;
+mod day21;
